5c30ec4e074b6031e683e3996d769fb5a7e7e583
Ashwin <40994846+AshwinNkl@users.noreply.github.com>
2018-07-12 13:09:30 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,5 +1,7 @@
 n=input()
 if(n=='a'or n=='e' or n=='i' or n=='o'or n=='u'):
     print("Vowel")
+elif(n=='A'or n=='E' or n=='I' or n=='O'or n=='U'):
+    print("Vowel")
 else:
     print("Consonant")
